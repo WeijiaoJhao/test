07b0360993d98f6ed0721528613bf55a9401c29f
WeijiaoJhao <1713161@gmail.com>
2019-03-06 22:35:13 +0800

    add sh

---
+++ tryy.sh
@@ -0,0 +1,14 @@
+tag=prod-`date +%Y%m%d%H%M`
+echo "start ${tag}!"
+# git checkout develop
+# git pull origin develop
+# git tag -a ${tag} -m ${tag}
+
+SHA1=`git log -1 --pretty=format:"%h"`
+echo "start ${SHA1}!"
+sed -i '' '/VUE_APP_TEST/d' ./.env.production
+echo "VUE_APP_TEST = 'tag ${tag}, SHA1 ${SHA1}'" >> ./.env.production
+
+yarn build
+serve -s dist
+echo "end"
