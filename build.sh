0bd57946ff46548f5035436a4460def2a6c11d13
WeijiaoJhao <1713161@gmail.com>
2019-04-08 22:06:57 +0800

    aaa

--- build.sh
+++
@@ -1,7 +0,0 @@
-SHA1=`git log -1 --pretty=format:"%h"`
-echo "start ${SHA1}!"
-echo "VUE_APP_TEST = 'SHA1 ${SHA1}'" >> ./.env.production
-
-vue-cli-service build
-
-sed -i '' '/VUE_APP_TEST/d' ./.env.production
