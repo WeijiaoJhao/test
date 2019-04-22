0a947e87368692762580078492a09e3897f9c31e
WeijiaoJhao <1713161@gmail.com>
2019-04-22 23:26:35 +0800

    add hash tag

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,26 @@
 const webpack = require("webpack");
 const path = require("path");
+const execSync = require("child_process").execSync;
 
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
+
+process.env.VUE_APP_GIT_HASH = execSync(
+  "git log -1 --pretty=format:'%h'"
+).toString();
+
+process.env.VUE_APP_GIT_TAG = (() => {
+  try {
+    return execSync("git describe --exact-match --abbrev=0")
+      .toString()
+      .replace("\n", "");
+  } catch {
+    return "";
+  }
+})();
+
+// console.log("www-3", process.env.VUE_APP_GIT_HASH, process.env.VUE_APP_GIT_TAG);
 
 module.exports = {
   configureWebpack: {
